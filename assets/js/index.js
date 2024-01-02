ec6205fbaacb7fd57fa3cbd6fda0daf53690948e
Sherhan Kantaev <kansherhan@gmail.com>
2024-01-02 21:04:38 +0600

    created repository

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+let splide = null;
+
+window.addEventListener("load", function () {
+  splide = new Splide(".splide", {
+    pagination: false,
+    type: "loop",
+    perPage: 1,
+    autoplay: true,
+    interval: 3500,
+  });
+
+  splide.mount();
+});
